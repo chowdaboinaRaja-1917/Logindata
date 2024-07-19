ef287d061e0f6dd4c6fb3c1e03783c6803a81b89
Your Name <rajachowdaboin3@gmail.com>
2024-07-19 21:47:46 +0530

    fr

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import { create, router as _router, defaults } from "json-server"; // importing json-server library
-const server = create();
-const router = _router("logindata.json");
-const middlewares = defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const jsonServer = require("json-server"); // Importing json-server library
 
-server.use(middlewares);
-server.use(router);
+const server = jsonServer.create(); // Creating a new json-server instance
+const router = jsonServer.router("logindata.json"); // Specifying the JSON file to be used as a data source
+const middlewares = jsonServer.defaults(); // Using default middlewares (logger, static, cors, no-cache)
 
-server.listen(port);
+const port = process.env.PORT || 8080; // Setting the port to the environment variable PORT or defaulting to 8080
+
+server.use(middlewares); // Applying default middlewares
+server.use(router); // Using the router to handle requests
+
+// Starting the server and listening on the specified port
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
