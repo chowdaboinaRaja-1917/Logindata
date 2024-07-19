60597d1b403a0e4e9da92491d183eb17b18d7458
chowdaboinaRaja-1917 <161336986+chowdaboinaRaja-1917@users.noreply.github.com>
2024-07-20 00:11:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = jsonServer.create(); // Creating a new json-server instance
 const router = jsonServer.router("logindata.json"); // Specifying the JSON file to be used as a data source
 const middlewares = jsonServer.defaults(); // Using default middlewares (logger, static, cors, no-cache)
 
-const port = process.env.PORT || 8080; // Setting the port to the environment variable PORT or defaulting to 8080
+const port = process.env.PORT || 3001; // Setting the port to the environment variable PORT or defaulting to 8080
 
 server.use(middlewares); // Applying default middlewares
 server.use(router); // Using the router to handle requests
